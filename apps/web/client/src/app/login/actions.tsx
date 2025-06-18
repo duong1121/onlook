a61923e8af16f4fcaa6cf5a2e4fca3e4d47cdf81
duong1121 <75771867+duong1121@users.noreply.github.com>
2025-06-18 16:22:32 +0700

    Update actions.tsx

--- apps/web/client/src/app/login/actions.tsx
+++ apps/web/client/src/app/login/actions.tsx
@@ -24,7 +24,7 @@ export async function login(provider: SignInMethod) {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider,
         options: {
-            redirectTo: `${origin}/auth/callback`,
+            redirectTo: `https://onlook-1.onrender.com/auth/callback`,
         },
     });
 
